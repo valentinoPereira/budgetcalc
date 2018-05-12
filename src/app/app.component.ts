81dd426a5c10db7e7ff7883e82c3e1ecd5833a78
Valentino Pereira <tino1000@live.com>
2018-05-12 20:50:03 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Budget Calculator';
+
+  income: number = 0;
 }
